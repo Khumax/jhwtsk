a53b5bf5bb46210c2a8f55f24765f965b72c839d
Khumax <m.khudoteplov@gmail.com>
2025-01-27 14:37:31 +0300

    first commit

---
+++ src/main/java/org/khumax/Epic.java
@@ -0,0 +1,23 @@
+package org.khumax;
+
+import lombok.Getter;
+
+@Getter
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        for (String subtask : subtasks) {
+            if (subtask.contains(query)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/org/khumax/Meeting.java
@@ -0,0 +1,28 @@
+package org.khumax;
+
+import lombok.Getter;
+
+@Getter
+public class Meeting extends Task {
+    protected String topic;
+    protected String project;
+    protected String start;
+
+    public Meeting(int id, String topic, String project, String start) {
+        super(id);
+        this.topic = topic;
+        this.project = project;
+        this.start = start;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        if (topic.contains(query)) {
+            return true;
+        } else if (project.contains(query)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
